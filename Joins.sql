f35a4740b4eadcbda83c7668665a908268a4f23f
suba2125 <subhasrip212007@gmail.com>
2026-08-08 22:03:20 +0530

    Add INNER JOIN query for IT department
    Added INNER JOIN query to filter employees by department.

--- Joins.sql
+++ Joins.sql
@@ -32,3 +32,12 @@ SELECT employees.name, departments.department_name
 FROM employees
 RIGHT JOIN departments
 ON employees.department_id = departments.department_id;
+
+
+-- JOIN + WHERE
+
+SELECT employees.name, departments.department_name
+FROM employees
+INNER JOIN departments
+ON employees.department_id = departments.department_id
+WHERE departments.department_name = 'IT';
