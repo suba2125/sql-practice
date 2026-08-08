cf415ab35a4eacafcf3a4f97f4b96f13b861d755
suba2125 <subhasrip212007@gmail.com>
2026-08-08 21:47:55 +0530

    Add SQL JOIN examples to Joins.sql
    Added SQL queries demonstrating INNER, LEFT, and RIGHT JOINs.

--- Joins.sql
+++ Joins.sql
@@ -1,4 +1,22 @@
+-- INNER JOIN
+
 SELECT employees.name, departments.department_name
 FROM employees
 INNER JOIN departments
+ON employees.department_id = departments.department_id;
+
+
+-- LEFT JOIN
+
+SELECT employees.name, departments.department_name
+FROM employees
+LEFT JOIN departments
+ON employees.department_id = departments.department_id;
+
+
+-- RIGHT JOIN
+
+SELECT employees.name, departments.department_name
+FROM employees
+RIGHT JOIN departments
 ON employees.department_id = departments.department_id;
