4d88c3cafbd3b3fefe15fe7fdcad0b38e71c4980
suba2125 <subhasrip212007@gmail.com>
2026-08-08 21:57:29 +0530

    Add LEFT JOIN and UNION to Joins.sql

--- Joins.sql
+++ Joins.sql
@@ -20,3 +20,15 @@ SELECT employees.name, departments.department_name
 FROM employees
 RIGHT JOIN departments
 ON employees.department_id = departments.department_id;
+
+SELECT employees.name, departments.department_name
+FROM employees
+LEFT JOIN departments
+ON employees.department_id = departments.department_id
+
+UNION
+
+SELECT employees.name, departments.department_name
+FROM employees
+RIGHT JOIN departments
+ON employees.department_id = departments.department_id;
